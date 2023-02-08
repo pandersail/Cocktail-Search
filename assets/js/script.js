dc0586912f5f633e3dc1d228e4ec4f5601e7050e
DaisyEverard <msdaisyeverard@gmail.com>
2023-02-08 21:09:02 +0000

    FEAT: basic ajax query

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,32 @@ getURLFunc = () => {
     let queryURL; 
     const category = categoryInput.val();
     let name = nameInput.val();
-    name = name.toLowerCase();  
-
     let ingredient = ingredientInput.val();
-    ingredient = ingredient.toLowerCase();
-    ingredient = ingredient[0].toUpperCase() + ingredient.slice(1)
 
     if (category) {
         queryURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
     }
     if (name) {
+        name = name.toLowerCase(); 
         queryURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
     }
     if (ingredient) {
+        ingredient = ingredient.toLowerCase();
+        ingredient = ingredient[0].toUpperCase() + ingredient.slice(1)
         queryURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
     }
+
     return queryURL; 
 }
 
 submitBtn.on('click', (event) => {
     event.preventDefault(); 
     let queryURL = getURLFunc(); 
+
+    $.ajax({
+        method: 'GET',
+        url: queryURL
+    }).then(response => {
+        console.log(response); 
+    })
 })
