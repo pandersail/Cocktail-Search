bfe619b07c990146edb418a2e69ee7d0e771f63c
DaisyEverard <msdaisyeverard@gmail.com>
2023-02-09 18:05:57 +0000

    FEAT: added async function with awaits to populate IDnums array

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,19 @@ const ingredientInput = $('#search-box-ingredient');
 const categoryInput = $('#search-box-category');
 const submitBtn = $('.submit-btn')
 
-getURLname = (name) => {
-    let name = nameInput.val();
+getURLName = (name) => {
     name = name.toLowerCase(); 
     return `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
     // gives full info about drink
 }
-getURLingredient = (ingredient) => {
-    let ingredient = ingredientInput.val();
+getURLIngredient = (ingredient) => {
     ingredient = ingredient.toLowerCase();
     ingredient = ingredient[0].toUpperCase() + ingredient.slice(1)
     // gives name, jpg src, and id
 }
-getURLcategory = (category) => {
+getURLCategory = (category) => {
     return `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`; 
     // gives name, jpg src, and id
-}
-getURLID = (ID) => {
-    return `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${ID}`
-    // gives full info about drink
 }
 
 const addImageFunc = (newRow, drink) => {
@@ -33,48 +27,120 @@ const addImageFunc = (newRow, drink) => {
 const addIngredientFunc = (newRow, drink) => {
     let ingredientCol = $('<div>');
     ingredientCol.attr('class', 'col col-md-6 col-lg-4');
-    newRow.append(ingredientCol)
+    let newList = $('<ul>')
+
+    // there are up to 15 ingredients in the API
+    for (let i = 1; i<16; i++) {
+        if (drink[`strIngreditent${i}`]) {
+            newList.append(`<li>${drink[`strIngreditent${i}`]}</li>`)
+        }
+    }
+
+    ingredientCol.append(newList);
+    newRow.append(ingredientCol);
 }
+
 const addRecipeFucn = (newRow, drink) => {
     let recipeCol = $('<div>'); 
     recipeCol.attr('class', 'col col-md-6 col-lg-4');
     newRow.append(recipeCol)
 }
 
-submitBtn.on('click', (event) => {
+submitBtn.on('click', async (event) => {
     event.preventDefault(); 
     let name = nameInput.val();
+    console.log('name: ' + name)
     let ingredient = ingredientInput.val();
+    console.log('ingredient: ' + ingredient)
     let category = categoryInput.val(); 
+    console.log('category: ' + category)
+    let IDnums = []; 
 
-    if (name) {
-       let queryURL =  getURLname(name); 
+    if (!name && !ingredient && !category) {
+        console.warn('!name && !ingredient && !category')
+        return 
     } else {
-        if (ingredient && !category) {
-           let queryURL = getURLingredient(ingredient); 
-        } else if (category && !ingredient) {
-           let queryURL = getURLcategory(category)
-        } else (catgory && ingredient) {
-
-        }
-    }
-
-    $.ajax({
-        method: 'GET',
-        url: queryURL
-    }).then(response => {
-        console.log(response)
-        for (let i = 0; i < 3; i++) {
-            let drink = response['drinks'][i]; 
-            console.log(drink)
-            let newRow = $('<div>'); 
-            newRow.attr('class', 'row');
+    if (name) {
+        console.warn('name only')
+        // get ids for first 3 containing search name
+        let response = await $.ajax({
+         method: 'GET',
+         url: getURLName(name)
+        })
+            for (let i = 0; i < 3; i++) {
+                IDnums.push(response['drinks'][i]['idDrink'])
+            }
+     } else {
+         if (ingredient && !category) {
+            console.warn('ingredient only')
+            // id for search by ingredients
+            let response = await $.ajax({
+                method: 'GET',
+                url: getURLIngredient(ingredient)
+            });
+                for (let i = 0; i < 3; i++) {
+                    IDnums.push(response['drinks'][i]['idDrink'])
+                }
+         } else if (category && !ingredient) {
+            console.warn('category only')
+             // id for search by category
+             let response = await $.ajax({
+                method: 'GET',
+                url: getURLCategory(category)
+            });
+                for (let i = 0; i < 3; i++) {
+                    IDnums.push(response['drinks'][i]['idDrink'])
+                }
+         } else {
+            console.warn('category and ingredient')
+             // if both category and ingredient
+             let ingArray = [];
+             let catArray = [];
+            let ingResponse = await $.ajax({
+                method: 'GET',
+                url: getURLIngredient(ingredient)
+            });
+                for (let i = 0; i < ingResponse['drinks'].length; i++) {
+                    ingArray.push(ingResponse['drinks'][i]['idDrink']); 
+                }
             
-            addImageFunc(newRow, drink);
-            addIngredientFunc(newRow, drink);
-            addRecipeFucn(newRow, drink); 
+            let catResponse = await $.ajax({
+                method: 'GET',
+                url: getURLCategory(category)
+            });
+                for (let i = 0; i < catResponse['drinks'].length; i++) {
+                    catArray.push(catResponse['drinks'][i]['idDrink']); 
+                }
 
-            $('.results').append(newRow)
-        }
-    })
+            ingArray.forEach(ingID => {
+                if (catArray.find(catID => catID === ingID)) {
+                    IDnums.push(item);
+                }
+            }) 
+            if (IDnums.length > 3) {
+                IDnums = IDnums.splice(0,2); 
+            }
+         } 
+     }
+ 
+     console.log(IDnums); 
+
+     IDnums.forEach(drinkID => {
+        $.ajax({
+            method: 'GET',
+            url: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkID}`
+        }).then(response => {
+                let drink = response['drinks'][0];
+                console.log(drink);
+                let newRow = $('<div>'); 
+                newRow.attr('class', 'row');
+                
+                addImageFunc(newRow, drink);
+                addIngredientFunc(newRow, drink);
+                addRecipeFucn(newRow, drink); 
+    
+                $('.results').append(newRow)    
+        })
+     })
+    }
 })
